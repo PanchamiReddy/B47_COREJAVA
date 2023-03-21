9f592226a98ce1769bb430ac7ceb38d87d087fae
TejV <TejV@DESKTOP-EVS3GAO>
2023-03-21 16:02:22 +0530

    Karnataka

---
+++ c2tc_vvit/src/com/tns/inheritance/Singledemo.java
@@ -0,0 +1,16 @@
+package com.tns.inheritance;
+
+public class Singledemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	India i=new India();
+//	i.displayNational();
+//		i.displayLocal();
+		Karnataka k=new Karnataka();
+		k.displayLocal();
+		k.displayNational();
+		
+	}
+
+}
